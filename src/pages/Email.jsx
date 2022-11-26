b2a2c58d427790f56b5fede7b104443dcd178a46
preete126 <ismailbarakatopeyemi05@gmail.com>
2022-11-26 15:01:55 +0100

    chore: Designed the mobile responsiveness for the email and Nfts components

--- src/pages/Email.jsx
+++ src/pages/Email.jsx
@@ -7,33 +7,40 @@ import mask from '../assets/mask.png'
 import sea from '../assets/sea.png'
 
 function Email() {
-    
+
     return (
         <>
-            <section className='d-flex p-5  pt-3' style={{fontFamily:"Calibri"}} >
-                <main className='pt-5 pb-4 w-75'  >
-                    <div className='pt-3  tw-font-semibold tw-text-6xl  tw-text-zinc-700 ' style={{ lineHeight: "80px"}}>Rent a <span className='text-primary ' style={{fontSize:"4.2rem"}}>Place</span> away from <span className='text-primary'  style={{fontSize:"4.2rem"}}>Home</span> in the <span className='text-primary'  style={{fontSize:"4.2rem"}}>Metaverse</span></div>
-                    <div className=' pt-5    fs-3 me-5 pe-5'>We provide you  access to luxury and affordable houses in the metaverse, get a chance to turn your imagination into reallity at your comfort zone </div>
-                    <form action="" className='pt-5 d-flex'>
-                        <input className='w-50 border border-2 form-control rounded-0 p-3 rounded-start  ' type="search" placeholder='search for location' />
-                        <button className='btn rounded-end rounded-0 p-2 px-5 btn-primary' style={{width:"35%"}}>Search</button>
+            <section className='d-flex px-5 pb-4  tw-pt-10 max-[1030px]:tw-pt-0 max-md:tw-flex-col' style={{ fontFamily: "Calibri" }} >
+                <main className='tw-pt-12 pb-4 '  >
+                    <div className='tw-pt-8  tw-font-semibold tw-text-6xl max-[1030px]:tw-text-5xl  tw-text-zinc-700 max-[915px]:tw-text-4xl max-[915px]:tw-pr-16 max-md:tw-text-6xl max-md:tw-pr-0 ' style={{ lineHeight: "80px" }}>
+                        Rent a <span className='text-primary ' >Place</span> away from <span className='text-primary' >Home</span> in the <span className='text-primary' >Metaverse</span>
+                    </div>
+                    <div className=' tw-pt-8  tw-pr-28 tw-text-2xl max-[1030px]:tw-text-xl max-[1030px]:tw-pr-10' style={{ fontFamily: "sans-serif" }} >We provide you  access to luxury and affordable houses in the metaverse, get a chance to turn your imagination into reallity at your comfort zone </div>
+                    <form action="" className='tw-pt-12 d-flex'>
+                        <input className=' border border-2 form-control rounded-0 p-3 rounded-start  ' type="search" placeholder='search for location' style={{ width: "50%" }} />
+                        <button className='btn rounded-end rounded-0 p-2 tw-text-center btn-primary tw-w-56 max-[1030px]:tw-w-48 max-[460px]:tw-w-44' >Search</button>
                     </form>
                 </main>
-                <div className=' tw-relative  ' style={{width:'550px'}} >
-                    <img className='tw-rounded-3xl  tw-absolute ' style={{ top: "140px" }} src={Image1} width={"220px"} alt="" />
-                    <img className='tw-rounded-3xl tw-absolute ' style={{ right: "0%", top: "50px" }} src={Image2} width={"220px"} alt="" />
-                    <img className='tw-rounded-3xl tw-absolute ' style={{ top: "345px" }} src={Image3} width={"220px"} alt="" />
-                    <img className='tw-rounded-3xl tw-absolute ' style={{ top: "260px", right: "0%" }} src={Image4} width={"220px"} alt="" />
+                <div className=' tw-relative d-flex gap-2 w-100 tw-pt-10  max-[1030px]:tw-pt-20 max-sm:tw-pt-5'>
+                    <div className='tw-pt-20 tw-mt-6 max-[480px]:tw-mt-0 max-[440px]:tw-pt-10'>
+                        <img className='tw-rounded-3xl tw-w-96   ' src={Image1} alt="" />
+                        <img className='tw-rounded-3xl tw-w-96  mt-2' src={Image4} alt="" />
+                    </div>
+                    <div>
+                        <img className='tw-rounded-3xl tw-w-96  ' src={Image3} alt="" />
+                        <img className='tw-rounded-3xl tw-w-96  mt-2' src={Image2} alt="" />
+                    </div>
+
                 </div>
             </section>
             <main className='bg-primary p-3 mt-5  d-flex tw-justify-around tw-text-white' style={{ fontFamily: "Arial, Helvetica, sans-serif", alignItems: "center" }}>
-                <div className='fs-3 tw-font-semibold d-flex gap-2'><img src={token} width={30} alt="" /> MBToken</div>
-                <div className=' fs-4 tw-font-medium d-flex gap-2' style={{ alignItems: "center", fontFamily: "'Courier New', Courier, monospace", wordSpacing: "-10px" }} > <img src={mask} width={40} alt="" />  M E T A M A S K</div>
-                <div className='fs-3 tw-font-semibold d-flex gap-2' style={{ alignItems: "center" }}> <img src={sea} width={35} alt="" />  OpenSea</div>
+                <div className='tw-text-3xl max-sm:tw-text-xl max-[500px]:tw-text-lg  tw-font-semibold d-flex gap-2 '><img src={token} className='tw-w-8 max-[580px]:tw-w-6 max-[440px]:tw-w-5 '  alt="" /> MBToken</div>
+                <div className=' tw-text-2xl max-sm:tw-text-xl max-[500px]:tw-text-lg tw-font-medium d-flex gap-2' style={{ alignItems: "center", fontFamily: "'Courier New', Courier, monospace", wordSpacing: "-10px" }} > <img src={mask} className='tw-w-10 max-[580px]:tw-w-7 max-[580px]:tw-w-6' alt="" />  M E T A M A S K</div>
+                <div className='tw-text-3xl max-sm:tw-text-xl  max-[500px]:tw-text-lg  tw-font-semibold d-flex gap-2' style={{ alignItems: "center" }}> <img src={sea} className='tw-w-10 max-[580px]:tw-w-6' alt="" />  OpenSea</div>
             </main>
             <div>
-                <h1 className='tw-font-bold tw-text-5xl text-center py-5'>Inspiration for your next adventure</h1>
-              
+                <h1 className='tw-font-bold tw-text-5xl max-[975px]:tw-text-4xl text-center py-5 inn'>Inspiration for your next adventure</h1>
+
             </div>
         </>
     );
--- src/pages/nfts.jsx
+++ src/pages/nfts.jsx
@@ -5,21 +5,21 @@ import img3 from "../assets/U.jpg"
 function Nfts() {
     return (
         <>
-            <section className="bg-primary px-5 d-flex tw-justify-between gap-5" style={{paddingTop:"80px",paddingBottom:"120px"}}>
-                <main className="pt-5 text-white w-75" style={{ fontFamily: "Verdana, Geneva, Tahoma, sans-serif" }}>
-                    <div className="mt-5 tw-text-5xl tw-font-semibold">Metabnb NFTs</div>
-                    <div className="py-5 fs-5 pe-5 me-5" style={{ lineHeight: "40px" }}>Discover our NFT gift cards collection. Loyal customers get amazing gift cards which are traded as NFTs. This NFTs gives our customers access to loads of our exclussive services.</div>
+            <section className="bg-primary tw-px-14   tw-flex gap-5 icons max-md:tw-block " style={{paddingTop:"80px",paddingBottom:"120px"}}>
+                <main className="pt-5 text-white w-75 wid" style={{ fontFamily: "Verdana, Geneva, Tahoma, sans-serif" }}>
+                    <div className="mt-5 tw-text-5xl tw-font-semibold max-[915px]:tw-text-4xl">Metabnb NFTs</div>
+                    <div className="py-5 tw-text-lg tw-mr-28 max-[1030px]:tw-mr-2" style={{ lineHeight: "40px" }}>Discover our NFT gift cards collection. Loyal customers get amazing gift cards which are traded as NFTs. This NFTs gives our customers access to loads of our exclussive services.</div>
                     <button className="btn btn-light text-primary p-2 px-4 tw-font-semibold">Learn more</button>
                 </main>
-                <main className="w-75">
-                    <div className="mt-5" style={{paddingLeft:"10rem"}} >
-                        <img className="rounded-3 tw-rotate-12 ms-5 " src={img1} width={"80%"} alt="" />
+                <main className="w-75 max-md:tw-flex  max-md:tw-gap-6 wid ">
+                    <div className="mt-5  tw-ml-28 max-[915px]:tw-ml-10 max-md:tw-ml-0" >
+                        <img className="rounded-3 tw-rotate-12 tw-mx-auto max-md:tw-w-96  tw-w-80 max-[1030px]:tw-w-60 max-[980px]:tw-w-52 max-[915px]:tw-w-44 " src={img1}  alt="" />
                     </div>
                     <div>
-                        <img className="rounded-3" style={{ transform: "rotate(340deg)",marginTop:"-6rem" }} src={img2} width={"60%"} alt="" />
+                        <img className="rounded-3 max-md:tw-w-96 tw-w-80 max-[1030px]:tw-w-60  max-[980px]:tw-w-52  max-[915px]:tw-w-44 fir" style={{ transform: "rotate(330deg)", marginTop:"-3rem"}} src={img2} alt="" />
                     </div>
-                    <div  style={{marginTop:"-11rem", paddingLeft:"15rem"}}>
-                        <img className="rounded-3 " style={{ transform: "rotate(350deg)" }} src={img3} width={"100%"} alt="" />
+                    <div className="tw-ml-48 max-[915px]:tw-ml-28  max-md:tw-ml-0">
+                        <img className="rounded-3 max-md:tw-w-96 tw-w-80 max-[915px]:tw-w-44 mx-auto sec " style={{ transform: "rotate(350deg)", marginTop:"-11.5rem" }} src={img3}  alt="" />
                     </div>
                 </main>
             </section>

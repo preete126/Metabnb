e600965529219754bcec223f61523005c439e0ce
preete126 <ismailbarakatopeyemi05@gmail.com>
2022-11-29 17:49:10 +0100

    chore: styled the navbar responsiveness and other components

--- src/404.jsx
+++ src/404.jsx
@@ -1,27 +1,11 @@
+import "./pages/default.css"
+import { Link } from "react-router-dom"
 function Page404() {
-    return ( <section>
-        <h1>Error</h1>
-<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-    </section> );
+    return (<section className="notFound">
+        <Link to={'/'}>
+            <button className="btn btn-primary fs-4 fw-bold w-25 tw-absolute" style={{ bottom: "2%", left: "50%", transform: "translate(-50%, -50%)" }} >Go Back</button>
+        </Link>
+    </section>);
 }
 
 export default Page404;

3acbbfee6cfb82e46798930ba18d6ffafcf90d16
preete126 <ismailbarakatopeyemi05@gmail.com>
2022-11-29 17:19:58 +0100

    feat: Added the connect wallet component

--- src/pages/Defaults.jsx
+++ src/pages/Defaults.jsx
@@ -2,6 +2,8 @@ import home from '../assets/home.png'
 import home1 from '../assets/home (1).png'
 import './default.css'
 import face from '../assets/face.png'
+import metafox from '../assets/metafox.png'
+import wallet from '../assets/wallet.png'
 import twitter from '../assets/twitter.png'
 import instagram from '../assets/instagram.png'
 import { Link } from "react-router-dom"
@@ -11,37 +13,59 @@ function Defaults({ children }) {
 
     return (
         <>
-            <nav className='d-flex tw-justify-between navs pt-4 pb-4 tw-px-12 max-[973px]:tw-px-6 '>
-                <div>
-                   <Link to={"/"} className='d-flex tw-text-4xl tw-font-bold  meta'> <img src={home} width={50} alt="" /> Metabnb</Link>
-                </div>
-                <div className='d-flex gap-5 tw-text-xl Menu' >
-                    <Link  className='text-decoration-none text-dark'>Home</Link >
-                    <Link to={"/places"} className='text-decoration-none text-dark'>Place to stay</Link >
-                    <a href='#' className='text-decoration-none text-dark'>NFTs</a >
-                    <a href='#' className='text-decoration-none text-dark'>Community</a >
-                </div>
-                <div className=' tw-text-base Menu'>
-                    <button className='btn btn-primary px-4 py-2' data-toggle="modal" data-target="#staticBackdrop">Connect wallet</button>
-                    <div className="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdrop" aria-hidden="true">
-                        <div className="modal-dialog">
-                            <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title" id="staticBackdropLabel">Modal title</h5>
-                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div className="modal-body">
+            <section className='tw-px-10 pt-2 max-[973px]:tw-px-3 '>
+                <nav className="navbar navbar-expand-lg  ">
+                    <div className="container-fluid">
+                        <Link to={"/"} className=' navbar-brand  fs-1  tw-font-bold  meta'> <img src={home} width={50} className='d-inline-block align-text-top' alt="" /> Metabnb</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                            <ul className="navbar-nav w-75 justify-content-center  tw-gap-8  tw-ml-3 max-[1030px]:tw-ml-1 max-[1000px]:tw-gap-6 max-[1000px]:tw-ml-0   me-auto mb-2 mb-lg-0  mt-0 mt-lg-2 max-[991px]:tw-gap-0" >
+                                <li className="nav-item">
+                                    <Link to={"/"} className=' nav-link active fs-5  ' aria-current="page">Home</Link >
+                                </li>
+                                <li className="nav-item">
+                                    <Link to={"/places"} className=' nav-link active fs-5  '>Place to stay</Link >
+                                </li>
 
+                                <li className="nav-item">
+                                    <a href='#' className=' nav-link active fs-5  '>NFTs</a >
+                                </li>
+                                <li className="nav-item">
+                                    <a href='#' className=' nav-link active fs-5  '>Community</a >
+                                </li>
+                            </ul>
+                            <div className=' tw-text-base Menu'>
+                                <button className='btn btn-primary px-4 py-2' data-bs-toggle="modal" data-bs-target="#exampleModal">Connect wallet</button>
+                                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div className="modal-dialog modal-dialog-centered">
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h5 className="modal-title tw-font-semibold" id="exampleModalLabel">Connect Wallet</h5>
+                                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div className="modal-body py-3">
+                                                <p>Choose your preferred wallet:</p>
+                                                <div className='form-group border border-1 bg-light rounded d-flex p-1 px-2'>
+                                                    <img src={metafox} width={44} loading='lazy' alt="" />
+                                                    <button className='btn  form-control p-2 text-start  d-flex  tw-justify-between fw-bold'> Metamask <span>⇨</span></button>
+                                                </div>
+                                                <div className='form-group border border-1 bg-light rounded mt-3 d-flex p-1 px-2'>
+                                                    <img src={wallet} width={44} loading='lazy' alt="" />
+                                                    <button className='btn  form-control p-2 text-start  d-flex  tw-justify-between fw-bold'> WalletConnect <span>⇨</span></button>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
                                 </div>
 
                             </div>
                         </div>
                     </div>
-
-                </div>
-            </nav>
+                </nav>
+            </section>
             <div>{children}</div>
             <footer className='tw-px-12 text-white tw-bg-black tw-py-20 max-[973px]:tw-px-6  '>
                 <main className='tw-flex tw-justify-between max-[670px]:tw-flex-wrap max-[670px]:tw-gap-5 '>
@@ -85,7 +109,7 @@ function Defaults({ children }) {
                         </div>
                     </details>
                 </main>
-                <div className='tw-text-xs ' style={{marginTop:"1rem"}}>© 2022 Metabnb. Coded by Ismail Mubarokah</div>
+                <div className='tw-text-xs ' style={{ marginTop: "1rem" }}>© 2022 Metabnb. Coded by Ismail Mubarokah</div>
 
             </footer>
         </>

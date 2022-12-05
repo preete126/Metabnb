d10e74313b5466fd292d90e5a4f1880d556b217a
preete126 <ismailbarakatopeyemi05@gmail.com>
2022-12-05 17:20:21 +0100

    fix: fixed directory issues

--- src/pages/Places.jsx
+++ src/pages/Places.jsx
@@ -1,5 +1,5 @@
-import Defaults from "./components/Defaults";
-import Houses from "./components/places_Array";
+import Defaults from "../components/Defaults";
+import Houses from "../components/places_Array";
 
 function Places() {
     return ( 
